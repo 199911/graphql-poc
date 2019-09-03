734fd0c8376d95f489aee7ada5066c6cf0f9f699
KU Lok Sun <sundaykuloksun@gmail.com>
2019-09-03 14:47:01 +0800

    refactor: extract typeDefs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('./typeDefs');
 const { filter, find } = require('ramda')
 
 // This is a (sample) collection of books we'll be able to query
@@ -29,30 +30,6 @@ const authors = [
     books: ['2'],
   },
 ];
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: Author
-  }
-
-  type Author {
-    name: String
-    books: [Book]
-  }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
-  type Query {
-    books: [Book]
-    authors: [Author]
-  }
-`;
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
