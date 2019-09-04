1f3f8babe9f7015783b84d8790d78a1fd0b185bc
KU Lok Sun <sundaykuloksun@gmail.com>
2019-09-04 17:51:42 +0800

    feat: implement tree schema

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { filter, find } = require('ramda')
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    id: '1',
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: '1',
-  },
-  {
-    id: '2',
-    title: 'Jurassic Park',
-    author: '2',
-  },
-];
-
-const authors = [
-  {
-    id: '1',
-    name: 'J.K. Rowling',
-    books: ['1'],
-  },
-  {
-    id: '2',
-    name: 'Michael Crichton',
-    books: ['2'],
-  },
-];
-
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
 
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: Author
-  }
-
-  type Author {
-    name: String
-    books: [Book]
+  type Node {
+    id: Int
+    leaves: [Node]!
   }
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
-    authors: [Author]
+    tree(
+      root: Int!,
+      nodes: [NodeInput]!
+    ): Node
+  }
+
+  input NodeInput {
+    id: Int!
+    leaves: [Int]!
   }
 `;
 
@@ -58,18 +30,18 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
-    authors: () => authors,
+    tree: (parent, args, context, info) => {
+      return {
+        id: 1,
+        leaves: []
+      };
+    }
   },
-  Author: {
-    books(author) {
-      return filter(book => book.author === author.id, books);
-    },
-  },
-  Book: {
-    author(book) {
-      return find( author => book.author === author.id, authors);
-    },
+  Node: () => {
+    return {
+      id: 1,
+      leaves: []
+    }
   }
 };
 
